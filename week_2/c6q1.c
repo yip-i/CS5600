21bc1602e06fd47fcb938031723b15f40d3d3d5c
yip-i <75112256+yip-i@users.noreply.github.com>
2021-09-18 14:51:58 -0700

    c6 hw

---
+++ week_2/c6q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include<errno.h>
+
+int main(int argc, char* argv[]) {
+    struct timeval start,stop;
+    double difference;
+
+    gettimeofday(&start, NULL);
+    int filedesc = open("/Users/ianyip/Documents/Coding/CS5600/week_2/test", O_RDWR | O_CREAT, 0000);
+    close(filedesc);
+    gettimeofday(&stop, NULL);
+    difference = (stop.tv_usec - start.tv_usec) * 100;
+    printf("difference: %f\n", difference);
+}
