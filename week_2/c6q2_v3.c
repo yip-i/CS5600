e7b1f0f9a0b524bc51cf2b8ea2e7db23ec03e7d2
Ian <yip.i@northeastern.edu>
2021-09-19 22:13:23 -0700

    correct version of c6q2

---
+++ week_2/c6q2_v3.c
@@ -0,0 +1,55 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include<errno.h>
+
+#include <sched.h>
+
+
+int main(int argc, char* argv[]) {
+    int fd[2];
+    struct timeval start,stop;
+    cpu_set_t my_set;
+    CPU_ZERO(&my_set);
+    CPU_SET(7,&my_set);
+    sched_setaffinity(0, sizeof(cpu_set_t), &my_set);
+
+
+    if (pipe(fd) == -1) {
+        printf("Error occured\n");
+    }
+
+    int pid = fork();
+    if (pid < 0) {
+        printf("fork error\n");
+    }
+    else if (pid == 0) {
+        close(fd[0]);
+
+        gettimeofday(&start,NULL);
+
+        long x = start.tv_usec;
+        write(fd[1], &x, sizeof(long));
+        close(fd[1]);
+        return 0;
+    } else {
+        wait(NULL);
+        gettimeofday(&stop, NULL);
+        long y;
+        close(fd[1]);
+        read(fd[0], &y, sizeof(long));
+        close(fd[0]);
+
+        long difference = stop.tv_usec - y;
+        printf("difference: %ld\n", difference * 100);
+    }
+    return 0;
+}
